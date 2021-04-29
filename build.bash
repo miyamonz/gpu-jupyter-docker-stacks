fb959f225a4ada6b8a5a96ca8d1e9ab4ed6abcc4
Koichi Miyamoto <m5813k@gmail.com>
2021-04-29 20:22:34 +0900

    fix

--- build.bash
+++ build.bash
@@ -1,9 +1,17 @@
 #!/bin/bash
 OWNER=miyamonz
+REPOROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-CUDA_VERSION=10.0
-TYPE=cudnn7-devel
-BASE_OS=ubuntu18.04
+require() {
+    local NAME=$1
+    if [ -z ${!NAME} ] ; then
+        echo specify $NAME 
+        exit 1
+    fi
+}
+require CUDA_TAG
+require SUFFIX
+ROOT_CONTAINER=nvidia/cuda:${CUDA_TAG}
 
 get_base_container() {
     if [[ $1 = "" ]]; then
@@ -12,37 +20,42 @@ get_base_container() {
     if [[ $1 = "base-notebook" ]]; then
         exit 1
     fi
-    cat docker-stacks/${1}/Dockerfile | grep 'ARG BASE_CONTAINER' | cut -d '=' -f 2 | cut -d / -f 2
+    cat $REPOROOT/docker-stacks/${1}/Dockerfile | grep 'ARG BASE_CONTAINER' | cut -d '=' -f 2 | cut -d / -f 2
 }
-# get_base_container minimal-notebook
+
+to_image_name() {
+    echo ${OWNER}/${1}-${SUFFIX}
+}
+to_build_arg() {
+    # split space and append --build-arg
+    echo "$@" | tr ' ' "\n" | awk '{print "--build-arg " $0}' | tr "\n" ' '
+}
 
 build() {
     local NB_NAME=$1
     if [[ $NB_NAME = "" ]]; then
-        echo build argument needed
+        echo notebook name required  
         exit 1
     fi
-    local CUDA_TAG=${CUDA_VERSION}-${TYPE}-${BASE_OS}
-    local SUFFIX="cuda$CUDA_TAG"
-    local IMAGE_NAME="${OWNER}/${NB_NAME}"-${SUFFIX}
+    local IMAGE_NAME=$(to_image_name $NB_NAME)
 
-    local ROOT_CONTAINER=nvidia/cuda:${CUDA_TAG}
-    local BUILD_ARG="ROOT_CONTAINER=${ROOT_CONTAINER}"
+    local BUILD_ARG=""
     if [[ $NB_NAME = "base-notebook" ]]; then
-        :
+        BUILD_ARG+=" ROOT_CONTAINER=${ROOT_CONTAINER}"
+        BUILD_ARG+=" PYTHON_VERSION=${PYTHON_VERSION:-default}"
     else
-        local BASE_CONTAINER=`get_base_container $NB_NAME`
+        local BASE_CONTAINER=$(get_base_container $NB_NAME)
         build $BASE_CONTAINER
-        BUILD_ARG+=" BASE_CONTAINER=${OWNER}/${BASE_CONTAINER}-${SUFFIX}"
+        BUILD_ARG+=" BASE_CONTAINER=$(to_image_name $BASE_CONTAINER)"
     fi
-    BUILD_ARG=`echo $BUILD_ARG | tr ' ' "\n" | awk '{print "--build-arg " $0}' | tr "\n" ' '`
+    BUILD_ARGS=$(to_build_arg $BUILD_ARG)
+
     echo building $IMAGE_NAME ....
-    local BUILD_COMMAND="docker build ${DARGS} ${BUILD_ARG} --rm --force-rm -t ${IMAGE_NAME}:latest ./docker-stacks/${NB_NAME}"
+    local BUILD_COMMAND="docker build ${DARGS} ${BUILD_ARGS} --rm --force-rm -t ${IMAGE_NAME}:latest $REPOROOT/docker-stacks/${NB_NAME}"
     echo $BUILD_COMMAND
     eval $BUILD_COMMAND
-    echo -n "Built image size: "
-    docker images ${IMAGE_NAME}:latest --format "{{.Size}}"
     echo built $IMAGE_NAME
     echo
 }
-build minimal-notebook
+
+build $1
--- tf1-notebook/build.bash
+++ tf1-notebook/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
+REPOROOT=$(realpath $(dirname $0)/..)
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 OWNER=miyamonz
 
 CUDA_VERSION=10.0
@@ -8,27 +8,14 @@ TYPE=cudnn7-devel
 BASE_OS=ubuntu18.04
 CUDA_TAG=${CUDA_VERSION}-${TYPE}-${BASE_OS}
 
-NB_NAME=tf1-notebook
+PYTHON_VERSION=3.7
+SUFFIX="py${PYTHON_VERSION}-cuda$CUDA_TAG"
 
-build() {
-    local SUFFIX="cuda$CUDA_TAG"
-    local IMAGE_NAME="${OWNER}/${NB_NAME}"-${SUFFIX}
+CUDA_TAG=${CUDA_TAG} \
+SUFFIX=${SUFFIX} \
+PYTHON_VERSION=${PYTHON_VERSION} \
+    $REPOROOT/build.bash scipy-notebook
 
-    local ROOT_CONTAINER=nvidia/cuda:${CUDA_TAG}
-    local BUILD_ARG="ROOT_CONTAINER=${ROOT_CONTAINER}"
-
-    local BASE_CONTAINER=scipy-notebook
-    BUILD_ARG+=" BASE_CONTAINER=${OWNER}/${BASE_CONTAINER}-${SUFFIX}"
-
-    BUILD_ARG=`echo $BUILD_ARG | tr ' ' "\n" | awk '{print "--build-arg " $0}' | tr "\n" ' '`
-
-    echo building $IMAGE_NAME ....
-    local BUILD_COMMAND="docker build ${DARGS} ${BUILD_ARG} --rm --force-rm -t ${IMAGE_NAME}:latest ${SCRIPTPATH}"
-    echo $BUILD_COMMAND
-    eval $BUILD_COMMAND
-    echo -n "Built image size: "
-    docker images ${IMAGE_NAME}:latest --format "{{.Size}}"
-    echo built $IMAGE_NAME
-    echo
-}
-build 
+BASE_CONTAINER=${OWNER}/scipy-notebook-${SUFFIX}
+IMAGE_NAME=${OWNER}/tf1-notebook-${SUFFIX}
+docker build --build-arg BASE_CONTAINER=$BASE_CONTAINER --rm --force-rm -t $IMAGE_NAME:latest $SCRIPTPATH
