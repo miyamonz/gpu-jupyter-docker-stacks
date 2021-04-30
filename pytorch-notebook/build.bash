7152e6aec3108602e88d6c04dbf8a7d09d8bae71
Koichi Miyamoto <m5813k@gmail.com>
2021-04-30 15:08:06 +0900

    add pytorch notebook

---
+++ pytorch-notebook/build.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+REPOROOT=$(realpath $(dirname $0)/..)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+OWNER=miyamonz
+
+CUDA_VERSION=10.2
+TYPE=cudnn7-devel
+BASE_OS=ubuntu18.04
+CUDA_TAG=${CUDA_VERSION}-${TYPE}-${BASE_OS}
+
+SUFFIX="cuda$CUDA_TAG"
+
+CUDA_TAG=${CUDA_TAG} \
+SUFFIX=${SUFFIX} \
+PYTHON_VERSION=${PYTHON_VERSION} \
+    $REPOROOT/build.bash scipy-notebook
+
+BASE_CONTAINER=${OWNER}/scipy-notebook-${SUFFIX}
+
+NB_NAME=pytorch-notebook
+IMAGE_NAME=${OWNER}/${NB_NAME}-${SUFFIX}
+docker build --build-arg BASE_CONTAINER=$BASE_CONTAINER --rm --force-rm -t $IMAGE_NAME:latest $SCRIPTPATH
+docker tag $IMAGE_NAME $OWNER/${NB_NAME}-gpu
