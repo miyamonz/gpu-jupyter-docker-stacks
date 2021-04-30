d7b8288fe149053522547cb8a531944fb1173a99
Koichi Miyamoto <m5813k@gmail.com>
2021-04-30 15:07:55 +0900

    fix

--- tf1-notebook/build.bash
+++ tf1-notebook/build.bash
@@ -17,6 +17,8 @@ PYTHON_VERSION=${PYTHON_VERSION} \
     $REPOROOT/build.bash scipy-notebook
 
 BASE_CONTAINER=${OWNER}/scipy-notebook-${SUFFIX}
-IMAGE_NAME=${OWNER}/tf1-notebook-${SUFFIX}
+
+NB_NAME=tf1-notebook
+IMAGE_NAME=${OWNER}/${NB_NAME}-${SUFFIX}
 docker build --build-arg BASE_CONTAINER=$BASE_CONTAINER --rm --force-rm -t $IMAGE_NAME:latest $SCRIPTPATH
-docker tag $IMAGE_NAME $OWNER/tf1-notebook-gpu
+docker tag $IMAGE_NAME $OWNER/${NB_NAME}-gpu
